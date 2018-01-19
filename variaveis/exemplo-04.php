bc640fad09cd3f0e0f846a56ddefa6b77acf1dbf
Jeremy Trindade <jeremytrindade@gmail.com>
2018-01-19 23:19:46 +0100

    s3a15

--- variaveis/exemplo-04.php
+++ variaveis/exemplo-04.php
@@ -1,7 +1,16 @@
 <?php
+///////////////variaveis predefinidas
 
-$nome = (int)$_GET["a"];
+//$nome = (int)$_GET["a"];
+//para isto funcionar, é preciso meter o URI ou seja ?a=1
 
-var_dump($nome);
 
-?>
+//var_dump($nome);
+
+$ip = $_SERVER["REMOTE_ADDR"];// serve para ver ip do utilizador.
+//o $_SERVER serve para ir buscar informação do ambiente e do servidor, o ambiente o utilizador.
+echo $ip;
+echo "<br>";
+$ap = $_SERVER["SCRIPT_NAME"];
+echo $ap;
+?>	
