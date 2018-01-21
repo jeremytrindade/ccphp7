ecf85d37889dde130c5a6f5b0f3a54b4f3d89035
Jeremy Trindade <jeremytrindade@gmail.com>
2018-01-21 18:49:15 +0100

    s3a18

---
+++ strings/exemplo-01.php
@@ -0,0 +1,20 @@
+<?php
+////// Conhecendo os operadores spaceship
+
+$nome = "Hcode";
+
+$nome2 = 'Treinamentos';
+
+var_dump($nome, $nome2);
+
+echo "<br>";
+
+echo "ABC $nome";
+
+echo "<br>";
+
+echo 'ABC $nome';
+
+echo "<br>";
+
+?>	
---
+++ strings/exemplo-02.php
@@ -0,0 +1,30 @@
+<?php
+////// Conhecendo os operadores spaceship
+
+$nome = "joão rangel";
+
+echo strtoupper($nome);
+
+echo "<br>";
+
+$nome = "joao rangel";
+
+echo strtoupper($nome);
+
+echo "<br>";
+
+echo $nome;
+
+echo "<br>";
+
+echo strtolower($nome);
+
+echo "<br>";
+
+echo ucwords($nome);
+
+echo "<br>";
+
+echo ucfirst($nome);
+
+?>	
